dc20aa9429e67a5faa2e0a7e84092eebc69afdbd
yogiset <the3verlastin9@gmail.com>
2023-10-17 14:45:04 +0700

    fix error

--- src/main/java/com/example/MyAccountantBackEnd/repository/UserRepository.java
+++ src/main/java/com/example/MyAccountantBackEnd/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-;import java.util.Optional;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
