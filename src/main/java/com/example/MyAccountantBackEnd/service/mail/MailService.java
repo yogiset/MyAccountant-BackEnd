316d6c3fd2a219b2fd719ad75e25005cbc71938d
yogiset <the3verlastin9@gmail.com>
2023-10-19 19:00:07 +0700

    update

--- src/main/java/com/example/MyAccountantBackEnd/service/mail/MailService.java
+++ src/main/java/com/example/MyAccountantBackEnd/service/mail/MailService.java
@@ -30,7 +30,7 @@ public class MailService {
     public void sendActivationEmail(User user) {
         log.info("inside sendActivationEmail");
         String token = jwtUtil.generateToken(user.getEmail(), user.getRole(), user.getName());
-        String verificationUrl = "http://myaccountant-backend-production.up.railway.app/user/register/accountVerification/" + token ;
+        String verificationUrl = "http://localhost:8082/user/register/accountVerification/" + token ;
 
 
         String subject = "Please Activate your Account";
@@ -63,7 +63,7 @@ public class MailService {
         helper.setFrom("MyAccountant@gmail.com");
         helper.setTo(to);
         helper.setSubject(subject);
-        String htmlMsg = "<p><b>Your Login details for MyAccountant Management System</b><br><b>Email: </b> " + to + " <br><b>Password: </b> " + password + "<br><a href=\"https://myaccounta-n.vercel.app/login\">Click here to login</a></p>" +"Don't forget to changes your password, thank you for using our service";
+        String htmlMsg = "<p><b>Your Login details for MyAccountant Management System</b><br><b>Email: </b> " + to + " <br><b>Password: </b> " + password + "<br><a href=\"http://localhost:3000/login\">Click here to login</a></p>" +"Don't forget to changes your password, thank you for using our service";
         message.setContent(htmlMsg,"text/html");
         mailSender.send(message);
 
